478adc1e1f5c05c020ad0fde9473678a36ed0297
Ernesto J. Pama Jr <ernie.pama@stt.cloud>
2018-12-01 14:06:40 +0800

    Fixed Schema creator using Symfony finder

--- Cassandra/ORM/Tools/SchemaCreate.php
+++ Cassandra/ORM/Tools/SchemaCreate.php
@@ -2,10 +2,16 @@
 
 namespace CassandraBundle\Cassandra\ORM\Tools;
 
+use CassandraBundle\Cassandra\ORM\EntityManager;
+use CassandraBundle\Cassandra\ORM\SchemaManager;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class SchemaCreate
 {
+    /** @var Container */
     private $container;
 
     public function __construct($container)
@@ -15,36 +21,30 @@ class SchemaCreate
 
     public function execute($connection = 'default')
     {
+        /** @var EntityManager $em */
         $em = $this->container->get(sprintf('cassandra.%s_entity_manager', $connection));
+
+        /** @var SchemaManager $schemaManager */
         $schemaManager = $em->getSchemaManager();
 
-        // Get all files in src/*/Entity directories
-        $path = $this->container->getParameter('kernel.root_dir').'/../src';
-        $iterator = new \RegexIterator(
-            new \RecursiveIteratorIterator(
-                new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS),
-                \RecursiveIteratorIterator::LEAVES_ONLY
-            ),
-            '/^.+'.preg_quote('.php').'$/i',
-            \RecursiveRegexIterator::GET_MATCH
-        );
-        foreach ($iterator as $file) {
-            $sourceFile = $file[0];
-            if (!preg_match('(^phar:)i', $sourceFile)) {
-                $sourceFile = realpath($sourceFile);
-            }
-            if (preg_match('/src\/.*Entity\//', $sourceFile)) {
-                $className = str_replace('/', '\\', preg_replace('/(.*src\/)(.*).php/', '$2', $sourceFile));
-                $metadata = $em->getClassMetadata($className);
-                $tableName = $metadata->table['name'];
-                $indexes = $metadata->table['indexes'];
-                $primaryKeys = $metadata->table['primaryKeys'];
+        $finder = new Finder();
+        $finder->in('src/Entity');
 
-                if ($tableName) {
-                    $schemaManager->dropTable($tableName);
-                    $schemaManager->createTable($tableName, $metadata->fieldMappings, $primaryKeys);
-                    $schemaManager->createIndexes($tableName, $indexes);
-                }
+        /** @var SplFileInfo $file */
+        foreach ($finder->files() as $file) {
+            preg_match('/namespace (.*);/', $file->getContents(), $match);
+            $namespace = array_pop($match);
+            $className = sprintf('%s\%s', $namespace, str_replace('.php', '', $file->getFilename()));
+
+            $metadata = $em->getClassMetadata($className);
+            $tableName = $metadata->table['name'];
+            $indexes = $metadata->table['indexes'];
+            $primaryKeys = $metadata->table['primaryKeys'];
+
+            if ($tableName) {
+                $schemaManager->dropTable($tableName);
+                $schemaManager->createTable($tableName, $metadata->fieldMappings, $primaryKeys);
+                $schemaManager->createIndexes($tableName, $indexes);
             }
         }
 
